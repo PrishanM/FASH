35973e05e8f9e68c7c7e2e8a684d1637ad0ea97c
PrishanM <wgprishanm@gmail.com>
2016-06-26 12:49:56 -0700

    Super Category util class

---
+++ app/src/main/java/com/evensel/android/fash/fragments/ShopSuperCategoriesFragment.java
@@ -0,0 +1,20 @@
+package com.evensel.android.fash.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Prishan Maduka on 6/26/2016.
+ */
+public class ShopSuperCategoriesFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
